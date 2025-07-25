51a6ce655dbe98a40efff58dd924882c8d1a8461
Dawitcoding <140941173+Dawitcoding@users.noreply.github.com>
2025-07-25 21:58:32 +0300

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./component/About";
 import Services from "./component/Services";
-import Destination from "./component/Destination";
 import Gallery from "./component/Gallery";
 import Testimonial from "./component/Testimonial";
 import Contact from "./component/Contact";
